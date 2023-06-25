523a16d2ee282085e429618843e0c6b011ec9671
Tamer Naimov <tamer.naimov@gmail.com>
2023-06-25 23:56:37 +0300

    My second Commit (I changed the whole code)

--- src/Main.java
+++ src/Main.java
@@ -47,6 +47,7 @@ public class Main {
                     }
                 }
             }
+            //
             for (int i = 0; i < 20; i++) {
                 System.out.println("yoo");
             }
