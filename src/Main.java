035c9312b010b2c64e1e83ecb3a5b937cb0387ae
Tamer Naimov <tamer.naimov@gmail.com>
2023-06-28 20:39:40 +0300

    Fixed errors, almost completed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     public static int generateFourDigitNumber() {
         List<Integer> digits = new ArrayList<>();
@@ -15,89 +13,87 @@ public class Main {
             result += digits.get(i) * placeValue;
             placeValue /= 10;
         }
-        for (int i = 0; i < 20; i++) {
-            System.out.println("yo");
-        }
-        return result;
-//small change
-        //second change
 
-        //this is my sixth commit testggg
-        //Another quick test
+        return result;
     }
     public static void getNums(String numString) {
         Scanner scanner = new Scanner(System.in);
         boolean trueorfalse = true;
+
         while (trueorfalse == true) {
             boolean Check = true;
             List<Integer> bulls = new ArrayList<>();
             List<Integer> cows = new ArrayList<>();
             List<String> Digits = new ArrayList<>();
-            System.out.println("Въведете число");
-            int guess = scanner.nextInt();
-            String guessString = Integer.toString(guess);
-            for (int i = 0; i < guessString.length(); i++) {
-                Digits.add(String.valueOf(guessString.charAt(i)));
-            }
-            //2673
-            for (int i = 0; i < Digits.size(); i++) {
-                for (int j = i + 1; j < Digits.size(); j++) {
-                    if (Digits.get(i).equals(Digits.get(j))) {
-                        Check = false;
-
-                    }
-                }
-            }
-            //
-            for (int i = 0; i < 20; i++) {
-                System.out.println("yoo");
-            }
-            if(guessString.equals(numString)) {
-                System.out.println("Спечелихте!");
-                System.exit(0);
-            }
-            boolean trueorfalse2 = true;
+            System.out.println("Enter a number");
             try {
+                int guess = scanner.nextInt();
+
+                String guessString = Integer.toString(guess);
+
                 for (int i = 0; i < guessString.length(); i++) {
-                    if (numString.charAt(i) == guessString.charAt(i)) {
-                        bulls.add(Character.getNumericValue(numString.charAt(i)));
-                    } else if (numString.contains(Character.toString(guessString.charAt(i)))) {
-                        cows.add(Character.getNumericValue(guessString.charAt(i)));
-                    }
+                    Digits.add(String.valueOf(guessString.charAt(i)));
                 }
 
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println(e);
-                if(Check == false) {
-                    System.out.print("Въвели сте еднакви цифри, опитайте пак");
+
+                //2673
+                boolean Check2 = true;
+
+                for (int i = 0; i < Digits.size(); i++) {
+                    for (int j = i + 1; j < Digits.size(); j++) {
+                        if (Digits.get(i).equals(Digits.get(j))) {
+                            Check = false;
+                        }
+                    }
                 }
-                trueorfalse2 = false;
+                //
+                boolean trueorfalse2 = true;
+                boolean notEnoughNums = true;
+                boolean stringType = true;
+
+                if (guessString.length() < 4) {
+                    notEnoughNums = false;
+                }
+                if (guessString.equals(numString)) {
+                    System.out.println("You won!");
+                    System.exit(0);
+                }
+
+                try {
+                    for (int i = 0; i < guessString.length(); i++) {
+                        if (numString.charAt(i) == guessString.charAt(i)) {
+                            bulls.add(Character.getNumericValue(numString.charAt(i)));
+                        } else if (numString.contains(Character.toString(guessString.charAt(i)))) {
+                            cows.add(Character.getNumericValue(guessString.charAt(i)));
+                        }
+                    }
+
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println(e);
+                    trueorfalse2 = false;
+                }
+
+                if (Check == false) {
+                    System.out.println("You entered the same numbers, please try again!");
+                }
+                if (notEnoughNums == false) {
+                    System.out.println("You have entered less than 4 digits, please try again!");
+
+                }
+                if (Check == true && trueorfalse2 == true && notEnoughNums == true) {
+                    System.out.println("You Have " + bulls.size() + " Bulls");
+                    System.out.println("You Have " + cows.size() + " Cows");
+                }
+            }catch (InputMismatchException e) {
+                System.out.println("Error: Expected an integer input.");
+                scanner.next();
             }
-            if(Check == false) {
-                System.out.println("Въвели сте еднакви цифри, опитайте пак");
-            }
-            if(Check == true && trueorfalse2 == true) {
-                System.out.println("Imate " + bulls.size() + " bika");
-                System.out.println("Imate " + cows.size() + " kravi");
-            }
+
+
+
         }
     }
-    public static boolean checkDigits(int num)  {
-        List<String> Digits = new ArrayList<>();
-        String numString = Integer.toString(num);
-        for (int i = 0; i < numString.length(); i++) {
-            Digits.add(String.valueOf(numString.charAt(i)));
-        }
-        //2673
-        for (int i = 0; i < Digits.size(); i++) {
-            for (int j = i+1; j < Digits.size(); j++) {
-                if(Digits.get(i)== Digits.get(j)) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+
     public static void ShowMenu() {
 
     }
